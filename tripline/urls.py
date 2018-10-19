f0575670717fd05b337a99b2698cc0c97cd33987
saianxie <846886126@qq.com>
2018-10-19 19:45:08 +0800

    2018/10/19晚上7点40分

--- tripline/urls.py
+++ tripline/urls.py
@@ -5,5 +5,7 @@ app_name = 'tripline'
 urlpatterns = [
     url(r'^show/', views.show, name='show'),
     url(r'^addcountry/', views.add, name='add'),
-
+    url(r'^gettimebytimeid\w*/(?P<timeid>\d*)/', views.gettime, name='gettime'),
+    url(r'^getcountrybychauid\w*/(?P<chauid>\d*)/', views.getcountry, name='getcountry'),
+    url(r'^gettraveldata\w*/', views.gettraveldata, name='gettraveldata'),
 ]
--- tripline/views.py
+++ tripline/views.py
@@ -4,12 +4,11 @@ from datetime import datetime, timedelta
 from django.http import HttpResponse, JsonResponse, response
 from tripline import models
 
+
 # Create your views here.
 
 def show(request):
     pass
-
-
 
 
 # 向chau表中插入这个洲的国家数据
@@ -27,4 +26,29 @@ def add(request):
             return JsonResponse({"code": "408"})
     else:
         return JsonResponse({"code": "408"})
+
+
+def gettime(request,timeid):
+    try:
+        time = models.triptime.objects.filter(id=timeid).values('time')
+        return HttpResponse(json.dumps(list(time), ensure_ascii=False))
+    except Exception as ex:
+        return JsonResponse({"code": "error"})
+
+
+def getcountry(request,chauid):
+    try:
+        country = models.chau.objects.get(id=chauid)
+        countrys=country.country_set.all().values('name')
+        return HttpResponse(json.dumps(list(countrys),ensure_ascii=False))
+    except Exception as ex:
+        return JsonResponse({"code": "error"})
+
+
+def gettraveldata(request):
+    try:
+        traveldata = models.tripline.objects.filter().values()
+        return HttpResponse(json.dumps(list(traveldata), ensure_ascii=False))
+    except Exception as ex:
+        return JsonResponse({"code": "error"})
 
